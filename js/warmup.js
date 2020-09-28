97396bc3d69eac8faf067233141d8264f6fac2a6
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2020-09-28 09:34:29 -0500

    created warmup files

---
+++ js/warmup.js
@@ -0,0 +1,14 @@
+"use strict";
+
+
+// console.log ("Hello Josh");
+// console.log ("Hello Steve");
+// console.log ("Hello Kobe");
+// console.log ("Hello Tyler");
+// console.log ("Hello Lebron");
+
+function sayHello(name){
+    console.log("Hello" + name);
+}
+ sayHello("Vivian")
+sayHello("Kaitlin")
