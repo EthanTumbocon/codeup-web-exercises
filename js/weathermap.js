aaf218de62ef8fd90088d6be02583c5dc203a282
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2020-11-18 12:35:17 -0600

    adding the draggable pin functionality

--- js/weathermap.js
+++ js/weathermap.js
@@ -1,65 +1,60 @@
-function(){
+(function () {
+
     "use strict";
 
-
     $(document).ready(function () {
-        "use strict";
         $.get("http://api.openweathermap.org/data/2.5/forecast", {
             APPID: OPEN_WEATHER_APPID,
             q: "San Antonio, US"
         }).done(function (data) {
             console.log(data);
         });
-        // $.get("http://api.openweathermap.org/data/2.5/weather", {
-        //     APPID: OPEN_WEATHER_APPID,
-        //     q: "San Antonio, US"
-        // }).done(function (data) {
-        //     console.log(data);
-        // });
+        $.get("http://api.openweathermap.org/data/2.5/weather", {
+            APPID: OPEN_WEATHER_APPID,
+            q: "San Antonio, US"
+        }).done(function (data) {
+            console.log(data);
+        });
 
-        // mapboxgl.accessToken = mapboxToken;
-        // let mapOptions = {
-        //     container: 'map',
-        //     style: 'mapbox://styles/mapbox/streets-v11',
-        //     center: [-98.4916, 29.4252],
-        //     zoom: 10
-        //
-        // }
-        // function geocode(search, token) {
-        //     var baseUrl = 'https://api.mapbox.com';
-        //     var endPoint = '/geocoding/v5/mapbox.places/';
-        //     return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
-        //         .then(function(res) {
-        //             return res.json();
-        //             // to get all the data from the request, comment out the following three lines...
-        //         }).then(function(data) {
-        //             return data.features[0].center;
-        //         });
-        // }
-        // let map = new mapboxgl.Map(mapOptions)
-        // geocode("San Antonio, TX", mapboxToken).then(function(result) {
-        //     console.log(result);
-        //     map.setCenter(result);
-        //     map.setZoom(8);
-        //     var popup = new mapboxgl.Popup()
-        //         .setHTML("Macho Libre");
-        //     var marker = new mapboxgl.Marker()
-        //         .setLngLat(result)
-        //         .setPopup(popup)
-        //         .addTo(map);
-        // });
+        mapboxgl.accessToken = mapboxToken;
+        var coordinates = document.getElementById('coordinates');
+        let mapOptions = {
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-98.4916, 29.4252],
+            zoom: 10
 
+        }
 
-    });
+        function geocode(search, token) {
+            var baseUrl = 'https://api.mapbox.com';
+            var endPoint = '/geocoding/v5/mapbox.places/';
+            return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+                .then(function (res) {
+                    return res.json();
+                    // to get all the data from the request, comment out the following three lines...
+                }).then(function (data) {
+                    return data.features[0].center;
+                });
+        }
 
+            var map = new mapboxgl.Map(mapOptions)
+            var marker = new mapboxgl.Marker({
+                draggable: true
+            })
+                .setLngLat(result)
+                .addTo(map)
+        function onDragEnd() {
+            var lngLat = marker.getLngLat();
+            coordinates.style.display = 'block';
+            coordinates.innerHTML =
+                'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
+        }
 
+        marker.on('dragend', onDragEnd);
+        //    need to make the marker draggable
+        //    need to make a new marker on click (event listener) and have it send coordinates to console
+        //    need to let it display on the
+        });
 
-
-
-
-
-
-
-
-
-}
+})();
